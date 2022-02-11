3e0cc20ee3522dbac521d3153bda355b0b3bde97
Brie Liddick <bricy18@gmail.com>
2022-02-10 21:57:14 -0500

    Adding the if-else_1 file

--- if_else.sh
+++ if_else.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-echo "Its is raining?"
+#Print a message to the user "Is it raining"
+echo "It is raining?[Y/N]"
 
 #store the user input inside  of a variable named IS_RAIN
-read  IS_RAIN
+read -a IS_RAIN
 
 #Start the if-else block to check it it's raining or not
-
-
-if [ $IS_RAIN == "Y"|| [ $IS_RAIN =="y" ]
+if  [ $IS_RAIN == "Y" ] || [ $IS_RAIN =="y" ]
 then
   echo "Stay at home!"
-elif [ $IS_RAIN == "N" ] || [ $IS_RAIN == 'n' ]
+elif [ $IS_RAIN == "N" ] || [ $IS_RAIN == "n" ]
 then
   echo "Go out!"
 else
-  echo "Invalid outpu!"
+  echo "Invalid output!"
 fi
